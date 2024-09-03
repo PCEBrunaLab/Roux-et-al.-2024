aa5eacac089e56fc8b3ee1e4ba8af17800157bfd
PCEBrunaLab <157609251+PCEBrunaLab@users.noreply.github.com>
2024-09-03 11:27:46 +0100

    Update emptyDrops.sh

--- Single_Cell_RNA_Sequencing/QC and Preprocessing/emptyDrops.sh
+++ Single_Cell_RNA_Sequencing/QC and Preprocessing/emptyDrops.sh
@@ -1,25 +1,18 @@
 #! /usr/bin/bash
-#SBATCH --job=emptyDrops
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=12
 #SBATCH --time=72:00:00
 #SBATCH --mem-per-cpu=12000
-#SBATCH --partition=smp
-#SBATCH --output=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J60_singlecell/logs/%x.%j-.out
-#SBATCH --error=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J60_singlecell/logs/%x.%j-.err
-
-#SBATCH --mail-user=sian.hamer@icr.ac.uk
-#SBATCH --mail-type=ALL
 
 # Set working directory
-BASE_DIR=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J60_singlecell
+BASE_DIR=...
 cd $BASE_DIR
 
 # Activate mamba env
 module use /opt/software/easybuild/modules/all/
 module load Mamba
 source ~/.bashrc
-mamba activate hotspot
+mamba activate ...
 
 ## loop over sample and run emptyDrops + cell filtering on each sample separately - returns an SCE object
 ## per sample
@@ -27,6 +20,7 @@ SRCDIR=$(echo $BASE_DIR/src/)
 OUTDIR=$(echo $BASE_DIR/SCE.dir/)
 UMI=500 # threshold for background empty droplets - this will depend heavily on the sequencing depth of the samples
 
+#conduct per Multiplexed pool
 sampdir=$BASE_DIR/Neuro_A/
 
 H5FILE=$(find $sampdir/outs/multi/count/ -name "*matrix.h5")
