00f9a8b652f9ca65da4f5504e97e116b175536f6
PCEBrunaLab <157609251+PCEBrunaLab@users.noreply.github.com>
2024-09-03 11:40:03 +0100

    Update bwa_alignment.sh

--- DNA_Cellecta_Barcodes/bwa_alignment.sh
+++ DNA_Cellecta_Barcodes/bwa_alignment.sh
@@ -1,23 +1,16 @@
 #! /usr/bin/bash
-#SBATCH --job=bwa_alignment
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --time=30:00:00
-#SBATCH --partition=compute
-#SBATCH --output=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J63_DNA_sequencing/logs/%x.%j-.out
-#SBATCH --error=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J63_DNA_sequencing/logs/%x.%j-.err
-
-#SBATCH --mail-user=sian.hamer@icr.ac.uk
-#SBATCH --mail-type=ALL
 
 # Activate mamba env
 module use /opt/software/easybuild/modules/all/
 module load Mamba
 source ~/.bashrc
-mamba activate hotspot
+mamba activate ...
 
 # Set working directory
-BASE_DIR=/data/scratch/DMP/DUDMP/PAEDCANC/shamer/J63_DNA_sequencing
+BASE_DIR=...
 cd $BASE_DIR
 
 srun Rscript $BASE_DIR"/bwa_alignment.R"
